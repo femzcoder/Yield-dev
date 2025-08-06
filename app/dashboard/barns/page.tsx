0b8f9ca2b2b4df90ef8289052f82b9a6dda1858f
Femi Akerele <lovinamercie@gmail.com>
2025-08-06 08:10:04 +0100

    updated

--- app/dashboard/barns/page.tsx
+++ app/dashboard/barns/page.tsx
@@ -2,7 +2,7 @@
 
 import { PrimaryTabContainer } from '@/components/Container'
 import React from 'react'
-import Customers from './tabpages/Customers'
+// import Customers from './tabpages/Customers'
 import Overview from './tabpages/Overview'
 import Queue from './tabpages/Partners'
 import Contacts from './tabpages/Contacts'
